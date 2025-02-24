7f46e833dbff77c287664e2e90d7758e9c82b072
dima20d22 <dimasurovatka22@gmail.com>
2025-02-24 10:45:58 +0100

    render done

--- script.js
+++ script.js
@@ -4,21 +4,41 @@ console.log(number);
 const input = document.querySelector('.inputAufgabe');
 const button = document.querySelector('.ButtonAufgabe');
 const list = document.querySelector('.listAufgabe');
-if (input && button) {
-    button === null || button === void 0 ? void 0 : button.addEventListener('click', function (e) {
+const task = [];
+if (input && button && list) {
+    button.addEventListener('click', function (e) {
         e.preventDefault();
-        const inputValue = input.value;
-        console.log(inputValue);
+        const inputValue = input.value.trim();
+        if (inputValue == '')
+            return;
+        task.push(inputValue);
+        render();
+        input.value = '';
     });
 }
 else {
-    console.log('.inputAufgabenicht gefunden!');
+    console.log('something went wrong!');
 }
 function render() {
-    if (list) {
-        list.innerHTML = '';
-    }
-    else {
-        console.log('.listAufgabe не найден!');
+    if (!list)
+        return;
+    list.innerHTML = '';
+    task.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.classList.add('elementsAufgabe');
+        li.textContent = task;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'delete Aufgabe';
+        deleteButton.classList.add('deleteButtonAufgabe');
+        deleteButton.addEventListener('click', () => {
+            deleteTask(index);
+        });
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+    });
+    function deleteTask(index) {
+        task.splice(index, 1);
+        render();
     }
 }
+console.log('dsa');
